c0e80764a9ad228e4fb364df21bc3720beb3a7b8
Tristan Jong <tristan.jong@dominos.com.au>
2023-03-28 13:30:47 +1000

    using overflow to clip images

--- App.js
+++ App.js
@@ -1,16 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Card from './Card';
 import OverlayProvider from './OverlayPrivder';
 
 export default function App() {
   return (
       <View style={styles.container}>
-        <OverlayProvider>
-          <View style={styles.bodyContainer}>
-            <Text>Open up App.js to start working on your app!</Text>
-            <StatusBar style="auto" />
-          </View>
-        </OverlayProvider>
+        <Card />
       </View>
   );
 }
@@ -27,6 +23,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     width: '100%',
-    height: '100%'
+    height: '100%',
   }
 });
